b492996a8d92c17081c9d060f03f3006a7c1765a
belemaaa <graceitamunoala@gmail.com>
2023-07-20 08:12:09 -0700

    get

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import PostRequest from './components/PostRequest';
+import GetRequest from './components/GetRequest';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<PostRequest/>}/>
           <Route path='/home' element={<Home/>} />
+          <Route path='/get' element={<GetRequest/>}/>
         </Routes>
       </Router>
     </div>
